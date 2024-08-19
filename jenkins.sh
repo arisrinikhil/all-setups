99b46f90cccd86ca2361a8248e1b29a29a5f123f
arisrinikhil <152692307+arisrinikhil@users.noreply.github.com>
2024-08-19 15:56:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
-amazon-linux-extras install epel -y
+amazon-linux-extras install openjdk-17-jre
 update-alternatives --config java
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
